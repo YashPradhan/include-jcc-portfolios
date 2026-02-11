6ee62d5c87f7ef2ff0672deafb0b4fecbd2f2467
munneth <mugill@ucdavis.edu>
2026-02-10 21:29:40 -0800

    test of pushing straight to main

--- app/(members)/munneth/page.jsx
+++ app/(members)/munneth/page.jsx
@@ -3,7 +3,7 @@ import styles from "./page.module.scss";
 export default function MunnethPage() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Munneth's Portfolio Testing the Repo.</h1>
+      <h1 className={styles.title}>Munneth's Portfolio Testing the Repo. Testing straight push to main.</h1>
       <p className={styles.description}>Welcome to my page! Edit this file to customize your portfolio.</p>
     </div>
   );

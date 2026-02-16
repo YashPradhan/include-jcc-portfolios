c5042e05a11380d187cd5b7d5be8261986e60732
tjlin653 <tonylin415@gmail.com>
2026-02-15 22:51:53 -0800

    updated text

--- app/(members)/tony/page.jsx
+++ app/(members)/tony/page.jsx
@@ -4,7 +4,7 @@ export default function TonyPage() {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Tony's Portfolio</h1>
-      <p className={styles.description}>hidden in plain sight...</p>
+      <p className={styles.description}>something is hidden in plain sight...</p>
     </div>
   );
 }

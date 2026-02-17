f14de90ba6ac1d4927bea7a28cf94ad08f91c46b
tjlin653 <tonylin415@gmail.com>
2026-02-17 11:05:02 -0800

    uni & major

--- app/(members)/tony/page.jsx
+++ app/(members)/tony/page.jsx
@@ -3,8 +3,14 @@ import styles from "./page.module.scss";
 export default function TonyPage() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Tony's Portfolio</h1>
-      <p className={styles.description}>something is hidden in plain sight...</p>
+      <div className={styles.header}>
+        <h1 className={styles.title}>Tony's Portfolio</h1>
+        <p className={styles.description}>things are hidden in plain sight...</p>
+      </div>
+      <div className={`${styles.box} ${styles.intro}`}>
+        <p className={styles.boxInfo}>University of California, Davis</p>
+        <p className={styles.boxInfo}>B.A. Design</p>
+      </div>
     </div>
   );
 }

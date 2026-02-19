cee4445bd041349e5249c11ff13901b9c4414659
tjlin653 <tonylin415@gmail.com>
2026-02-18 21:20:52 -0800

    3x2 grid completed

--- app/(members)/tony/page.jsx
+++ app/(members)/tony/page.jsx
@@ -9,23 +9,29 @@ export default function TonyPage() {
       </div>
       <div className={styles.gridContainer}>
         <div className={`${styles.box} ${styles.intro}`}>
-        <p className={styles.boxInfo}>UC Davis</p>
-        <p className={styles.boxInfo}>B.A. Design</p>
-      </div>
-      <div className={`${styles.box} ${styles.interests}`}>
-        <p className={styles.boxInfo}>Interests:</p>
-        <p className={styles.boxInfo}>concept art, photography, game development</p>
-      </div>
-      <div className={`${styles.box} ${styles.work}`}>
-        <p className={styles.boxInfo}>
-          <a className={styles.link} href="https://github.com/tjlin653">github.com/tjlin653</a>
-        </p>
-        <p className={styles.boxInfo}><em>Death to Dragons</em> (turn-based card game)</p>
-        <p className={styles.boxInfo}><em>Website Master Controls</em> (accessibility-focused chrome extension)</p>
-      </div>
-      <div className={`${styles.box} ${styles.joke}`}>
-        <p className={styles.boxInfo}>"Always remember that you are unique. Just like everyone else."</p>
-      </div>
+          <p className={styles.boxInfo}>UC Davis</p>
+          <p className={styles.boxInfo}>B.A. Design</p>
+        </div>
+        <div className={`${styles.box} ${styles.interests}`}>
+          <p className={styles.boxInfo}>Interests:</p>
+          <p className={styles.boxInfo}>concept art, photography, game development</p>
+        </div>
+        <div className={`${styles.box} ${styles.joke}`}>
+          <p className={styles.boxInfo}>"Always remember that you are unique. Just like everyone else."</p>
+        </div>
+        <div className={`${styles.box} ${styles.work}`}>
+          <p className={styles.boxInfo}>
+            <a className={styles.link} href="https://github.com/tjlin653">github.com/tjlin653</a>
+          </p>
+        </div>
+        <div className={`${styles.box} ${styles.deathToDragons}`}>
+          <p className={styles.boxInfo}><em>Death to Dragons</em></p>
+          <p className={styles.boxInfo}>(turn-based card game)</p>
+        </div>
+        <div className={`${styles.box} ${styles.websiteControls}`}>
+          <p className={styles.boxInfo}><em>Website Master Controls</em></p>
+          <p className={styles.boxInfo}>(accessibility-focused chrome extension)</p>
+        </div>
       </div>
     </div>
   );
